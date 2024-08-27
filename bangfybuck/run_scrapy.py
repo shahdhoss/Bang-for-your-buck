62bf1016d880f9991625262e5c6abacc97cc13b7
shahdhoss <s-shahd.fouda@zewailcity.edu.eg>
2024-08-27 16:49:56 +0300

    python script for replacing using scrapy's cli

---
+++ bangfybuck/run_scrapy.py
@@ -0,0 +1,10 @@
+from scrapy.crawler import CrawlerProcess
+from spiders.scraping import noonSpider, AmazonSpider
+from scrapy.utils.project import get_project_settings
+
+item="jewelery"
+settings = get_project_settings()
+process = CrawlerProcess(settings)
+process.crawl(noonSpider, item=item)
+process.crawl(AmazonSpider, item=item)
+process.start()  
